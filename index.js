b268bd7bb765538edb13274834df1f7920b07ef9
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-26 01:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"]
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  kittens.concat(name)
+}
+
+function prependKitten(name) {
+  kittens.slice(name)
+  
+}
